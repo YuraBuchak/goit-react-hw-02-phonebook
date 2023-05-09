a345a7d7e8f70b4ab4bc17e6bea0a47d665b4851
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-05-09 16:33:44 +0300

    тест

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,11 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      React homework template це я пробую чи деплоїть, бо я замахався з тим
+      гітом
     </div>
   );
 };
